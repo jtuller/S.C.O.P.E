af87bf9e6dc24d7de0e07dbbd68c588ee940da2a
LeviRoss2 <30361605+LeviRoss2@users.noreply.github.com>
2020-10-01 20:36:38 -0500

    Fixed mil science handling, removed city items from army EITS's

--- commands/eits.js
+++ commands/eits.js
@@ -20,7 +20,7 @@ function raceIdentify(i) {
   }
 }
 
-function NameIdentify(i){
+function NameIdentify(i) {
   let Owner, Target;
   if (i.search() > 0) {
     return "human";
@@ -41,64 +41,61 @@ module.exports = (client, message, args) => {
   let GTs = 0;
   let extraArmies = 0;
   let extraTroops = 0;
-  let Type,interm,Owner,Target,tempEndOwner,startTarget,endTarget,startOwner,endOwner,tempOwner;
+  let tempArray,Type, interm, Owner, Target, tempEndOwner, startTarget, endTarget, startOwner, endOwner, tempOwner;
 
-  if(args[0] > 0 && args [1] > 0){
+  if (args[0] >= 0 && args[1] >= 0) {
     military = args[0];
     magic = args[1];
-  }
+    tempArray = args.slice(2,args.length).toString().split(`,`);
+  }else{tempArray = args.toString().split(`,`);}
 
   // Army EITS reports
-  if(args.length < 10){
+  if (args.length < 10) {
 
     Type = 'Army';
     rawUnits = args.toString().split('\n').slice(1).toString().split(':').toString().split(',');
-  
-    tempArray = args.toString().split(`,`);
 
     interm = tempArray.indexOf('from');
-    tempEndOwner = tempArray[tempArray.length-1].split(`\n`)[0].replace(':','');
+    tempEndOwner = tempArray[tempArray.length - 1].split(`\n`)[0].replace(':', '');
 
-    Target = tempArray.slice(0,interm).join(' ');
-    tempOwner = tempArray.slice(interm+1,tempArray.length-1);
+    Target = tempArray.slice(0, interm).join(' ');
+    tempOwner = tempArray.slice(interm + 1, tempArray.length - 1);
     tempOwner.push(tempEndOwner);
-  
+
     Owner = tempOwner.join(' ');
   }
   // City EITS reports
-  else{
+  else {
 
     Type = 'City';
-    rawUnits = args.toString().split(`\n`).slice(1,7).toString().split(`,`).toString().split(`:`).toString().split(`,`);
-    rawGTs = args.toString().split(`\n`).slice(12,13).toString().split(`,`).slice(1,2).toString().split(`:`);
-    rawUnknown = args.toString().split(`\n`).slice(19,20).toString().split(`,`);
+    rawUnits = args.toString().split(`\n`).slice(1, 7).toString().split(`,`).toString().split(`:`).toString().split(`,`);
+    rawGTs = args.toString().split(`\n`).slice(12, 13).toString().split(`,`).slice(1, 2).toString().split(`:`);
+    rawUnknown = args.toString().split(`\n`).slice(19, 20).toString().split(`,`);
     GTs = rawGTs[1];
     extraArmies = parseInt(rawUnknown[4]);
     extraTroops = parseInt(rawUnknown[6]);
 
-    rawUnits.push("GTs",GTs);
-
-    tempArray = args.toString().split(`,`);
+    rawUnits.push("GTs", GTs);
 
     startTarget = tempArray.indexOf('about');
     endTarget = tempArray.indexOf('');
     startOwner = tempArray.indexOf('by');
     // last part of Owner name
-    tempEndOwner = tempArray[tempArray.length - 11].split(`\n`)[0].replace(':','');
-    endOwner = tempArray.length-11;
-  
-    Target = tempArray.slice(startTarget+1,endTarget).join(' ');
-    tempOwner = tempArray.slice(startOwner+1,endOwner);
+    tempEndOwner = tempArray[tempArray.length - 11].split(`\n`)[0].replace(':', '');
+    endOwner = tempArray.length - 11;
+
+    Target = tempArray.slice(startTarget + 1, endTarget).join(' ');
+    tempOwner = tempArray.slice(startOwner + 1, endOwner);
     tempOwner.push(tempEndOwner);
-  
+
     Owner = tempOwner.join(' ');
   }
-  
+
   console.log(Target);
   console.log(Owner);
 
-  for(i=0; i<(rawUnits.length/2); i++){
-    units[i] = rawUnits[(i*2)+1];
+  for (i = 0; i < (rawUnits.length / 2); i++) {
+    units[i] = rawUnits[(i * 2) + 1];
   }
 
   // Identifies Race
@@ -116,44 +113,83 @@ module.exports = (client, message, args) => {
     let unitI = "u".concat(i);
     if (i < 7) {
       // units are base 0, so subtract 1
-      op = isNaN(units[i-1]) ? op : op + units[i-1] * race[unitI].op;
-      dp = isNaN(units[i-1]) ? dp : dp + units[i-1] * race[unitI].dp;
-      cost = isNaN(units[i-1]) ? cost : cost + units[i-1] * race[unitI].cost;
+      op = isNaN(units[i - 1]) ? op : op + units[i - 1] * race[unitI].op;
+      dp = isNaN(units[i - 1]) ? dp : dp + units[i - 1] * race[unitI].dp;
+      cost = isNaN(units[i - 1]) ? cost : cost + units[i - 1] * race[unitI].cost;
     }
   }
   op = military ? op * (1 + military / 10) : op;
   dp = military ? dp * (1 + military / 10) : dp;
-  dp = units[6] > 0 ? dp + units[6] * (5 + military) : dp;  // likewise, 6 would be GTs now, not 7
+  dp = units[6] > 0 ? dp + units[6] * (5 + parseInt(military)) : dp;  // likewise, 6 would be GTs now, not 7
 
-  //Output results
-  message.channel.send({
-    embed: {
-      color: 2123412,
-      description: ` EITS Requested by: ${message.author}\n
+  if (Type == 'City') {
+    //Output results
+    message.channel.send({
+      embed: {
+        color: 2123412,
+        description: ` EITS Requested by: ${message.author}\n
             **Target ${Type}:**
             ${Target}
             **Owner:**
             ${Owner}
 
             **Units**
-            ${race.u1.name}: ${numeral(units[0]).format("0,0")}\n${race.u2.name
-        }: ${numeral(units[1]).format("0,0")}\n${race.u3.name}: ${numeral(
-          units[2]
-        ).format("0,0")}\n${race.u4.name}: ${numeral(units[3]).format(
-          "0,0"
-        )}\n${race.u5.name}: ${numeral(units[4]).format("0,0")}\n${race.u6.name
-        }: ${numeral(units[5]).format("0,0")}\n${race.u7.name}: ${numeral(
-          units[6]
-        ).format("0,0")}
-            \n**Power**
-            Military Sci: ${military} \nMagic Sci: ${magic} \nCost: ${numeral(
-          cost
-        ).format("0,0")}\nOP: ${numeral(op).format("0,0.0")}\nDP: ${numeral(
-          dp
-        ).format("0,0.0")}
-            \n **credit**
+            ${race.u1.name}: ${numeral(units[0]).format("0,0")}
+            ${race.u2.name}: ${numeral(units[1]).format("0,0")}
+            ${race.u3.name}: ${numeral(units[2]).format("0,0")}
+            ${race.u4.name}: ${numeral(units[3]).format("0,0")}
+            ${race.u5.name}: ${numeral(units[4]).format("0,0")}
+            ${race.u6.name}: ${numeral(units[5]).format("0,0")}
+            ${race.u7.name}: ${numeral(units[6]).format("0,0")}
+            Extra Armies: ${numeral(extraArmies).format("0")}
+            Extra Troops: ${numeral(extraTroops).format("0,0")}
+            
+            **Power**
+            Military Sci: ${military}
+            Magic Sci: ${magic}
+            Cost: ${numeral(cost).format("0,0")}
+            OP: ${numeral(op).format("0,0.0")}
+            DP: ${numeral(dp).format("0,0.0")}
+            
+            **Credit**
             made with :heart: by Percy & Moff`,
-    },
-  });
-  message.delete({ timeout: 1000 });
+      },
+    });
+    message.delete({ timeout: 1000 });
+
+  } else {
+
+    //Output results
+    message.channel.send({
+      embed: {
+        color: 2123412,
+        description: ` EITS Requested by: ${message.author}\n
+            **Target ${Type}:**
+            ${Target}
+            **Owner:**
+            ${Owner}
+
+            **Units**
+            ${race.u1.name}: ${numeral(units[0]).format("0,0")}
+            ${race.u2.name}: ${numeral(units[1]).format("0,0")}
+            ${race.u3.name}: ${numeral(units[2]).format("0,0")}
+            ${race.u4.name}: ${numeral(units[3]).format("0,0")}
+            ${race.u5.name}: ${numeral(units[4]).format("0,0")}
+            ${race.u6.name}: ${numeral(units[5]).format("0,0")}
+            
+            **Power**
+            Military Sci: ${military}
+            Magic Sci: ${magic}
+            Cost: ${numeral(cost).format("0,0")}
+            OP: ${numeral(op).format("0,0.0")}
+            DP: ${numeral(dp).format("0,0.0")}
+            
+            **Credit**
+            made with :heart: by Percy & Moff`,
+      },
+    });
+    message.delete({ timeout: 1000 });
+
+  }
+
 }
